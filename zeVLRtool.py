996155f916d450dee6e86ee72ae26712c83039eb
Silous <silous666@gmail.com>
2023-10-28 15:59:34 +0200

    gitignore et zeVLRtool fonctionnel

---
+++ zeVLRtool.py
@@ -0,0 +1,32 @@
+import subprocess
+
+
+tool_vlr = "VLR_patch_data\\zevlr.exe "
+
+def unpack_ze2_bin():
+    """décompile le bin de vlr"""
+    subprocess.run(
+        tool_vlr + "unpack",
+        shell=True, creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+def creation_krlist():
+    """fait le fichier krlist de font"""
+    subprocess.run(
+        tool_vlr + "krlist",
+        shell=True, creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+def patch_ze2_bin():
+    """patch le bin de vlr"""
+    subprocess.run(
+        tool_vlr + "patch",
+        shell=True, creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+def repack_ze2_bin():
+    """repack le bin de vlr"""
+    subprocess.run(
+        tool_vlr + "repack",
+        shell=True, creationflags=subprocess.CREATE_NO_WINDOW,
+    )
